1599a89f3855c6332832319eb8cdd3f878ae85f6
aprizal darius <aprizaldarius@gmail.com>
2019-06-24 15:53:19 +0700

    abdi

---
+++ app/src/main/java/com/marketing/abdiubl/ActivityLogin.java
@@ -0,0 +1,129 @@
+package com.marketing.abdiubl;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.marketing.abdiubl.activity.BerandaActivity;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivityLogin extends AppCompatActivity {
+EditText etNip;
+Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        etNip = (EditText) findViewById(R.id.etNip);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser(etNip.getText().toString().trim());
+            }
+        });
+
+
+    }
+
+    private void loginUser(final String nip) {
+//Toast.makeText(getApplication(),"Data "+username,Toast.LENGTH_LONG).show();
+
+        class LoginAsync extends AsyncTask<String, Void, String> {
+
+            private Dialog loadingDialog;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loadingDialog = ProgressDialog.show(ActivityLogin.this, "Please wait", "Loading...",false,false);
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+
+                InputStream is = null;
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("username", nip));
+                String result = null;
+
+                try{
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpPost httpPost = new HttpPost(
+                            "https://ppikubl.com/api_absensi/login.php");
+                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                    HttpResponse response = httpClient.execute(httpPost);
+
+                    HttpEntity entity = response.getEntity();
+
+                    is = entity.getContent();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"), 8);
+                    StringBuilder sb = new StringBuilder();
+
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        sb.append(line + "\n");
+                    }
+                    result = sb.toString();
+                } catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result){
+                String s = result.trim();
+                loadingDialog.dismiss();
+                if(s.equalsIgnoreCase("success")){
+                    Intent intent = new Intent(ActivityLogin.this, BerandaActivity.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplication(),"Username atau Password Salah!",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        LoginAsync la = new LoginAsync();
+        la.execute(nip);
+    }
+
+}
---
+++ app/src/main/java/com/marketing/abdiubl/activity/BerandaActivity.java
@@ -0,0 +1,16 @@
+package com.marketing.abdiubl.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.marketing.abdiubl.R;
+
+
+public class BerandaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beranda);
+    }
+}
